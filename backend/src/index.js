73b83db06242f54c2cd734800119ec7f890515b4
Upekka Chakma <upekkachakma@gmail.com>
2024-03-22 20:35:48 +0600

    no need .then().catch() as connectDB() handling all the tasks and errors by itself using tryCatch in 'db' folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,7 @@
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-connectDB()
-  .then(() => {
-    const port = process.env.PORT || 7000;
-    app.listen(port);
-    console.log("app is running on port:", port);
-  })
-  .catch((error) => {
-    console.log("Something went wrong with PORT listening.", error);
-  });
+connectDB();
+const port = process.env.PORT || 7000;
+app.listen(port);
+console.log("app is running on port:", port);
